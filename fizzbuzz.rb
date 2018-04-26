a71fe50af6207abd05a005865ce6073559a38b19
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-26 23:06:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(num)
- if num % 2 == 0
-   return 'Fizz'
- elsif num % 3 == 0
-   return 'Buzz'
- elsif num % 3 == 0 && num % 2 == 0
+ if num % 3 == 0 && num % 2 == 0
    return 'FizzBuzz'
+ elsif num % 2 == 0
+     return 'Fizz'
+ elsif num % 3 == 0
+     return 'Buzz'
  else
    return nil
  end
